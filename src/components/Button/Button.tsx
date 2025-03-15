bdc0776997f9f8519e383555c6294418468e2928
Neal <info@nealbuerger.com>
2025-03-15 22:36:37 +0100

    chore: update dependencies and fix linting issues in components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import type React from "react";
 import styled from "styled-components";
 
 type colors = "default" | "primary";
@@ -10,6 +10,7 @@ const StyledButton = styled.button`
   font-weight: bold;
   cursor: pointer;
 `;
+
 const PrimaryButton = styled(StyledButton)`
   background: blue;
 `;
--- src/pages/overview/Overview.tsx
+++ src/pages/overview/Overview.tsx
@@ -6,13 +6,13 @@ import { LessonCard } from "./components/LessonCard";
  * Renders the overview page.
  */
 export const Overview: React.FC = () => {
-  if (!lessonData || !Array.isArray(lessonData)) {
-    return <div>Error loading lessons.</div>;
-  }
+	if (!lessonData || !Array.isArray(lessonData)) {
+		return <div>Error loading lessons.</div>;
+	}
 
-  const lessonCardComponents = lessonData.map((singleLesson) => (
-    <LessonCard key={singleLesson.id} lessonData={singleLesson} />
-  ));
+	const lessonCardComponents = lessonData.map((singleLesson) => (
+		<LessonCard key={singleLesson.id} lessonData={singleLesson} />
+	));
 
-  return <div>{lessonCardComponents}</div>;
+	return <div>{lessonCardComponents}</div>;
 };
